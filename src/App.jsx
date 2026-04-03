daf1fe8a3efe2e2735c2646a67e80ab020aa08da
Siddhi <siddhishirdhankar9824@gmail.com>
2026-04-03 20:38:28 +0530

    feat: implement controlled input using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
+
+
+
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="app">
       <h1>Streamify</h1>
 
       <div className="search">
-        <input placeholder="Search for movies..." />
+        <input placeholder="Search for movies..." 
+        value={searchTerm}
+        onChange={(e)=>setSearchTerm(e.target.value)}
+        />
       </div>
+      <p>Searching for: {searchTerm}</p>
     </div>
   );
 }
