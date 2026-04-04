776fa7dbd84dd9184203d670e9128eb35895fb0e
Siddhi <siddhishirdhankar9824@gmail.com>
2026-04-04 22:37:15 +0530

    feat: enhance UX with loading state, enter key search and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
+
+
 import { useState } from "react";
 import MovieCard from "./components/MovieCard";
+
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=2d91e9ce";
 
-
-
-
-
 function App() {
-
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
+    if (!title) return;
 
-    setMovies(data.Search || []);
+    try {
+      setLoading(true);
+
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+
+      setMovies(data.Search || []);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
     <div className="app">
       <h1>Streamify</h1>
 
       <div className="search">
-        <input placeholder="Search for movies..."
+        <input
+          placeholder="Search for movies..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              searchMovies(searchTerm);
+            }
+          }}
         />
 
         <button onClick={() => searchMovies(searchTerm)}>
@@ -32,14 +48,18 @@ function App() {
         </button>
       </div>
 
-      <div>
+      {/* Conditional Rendering */}
+      {loading ? (
+        <p>Loading...</p>
+      ) : movies.length > 0 ? (
         <div className="movies">
-  {movies.map((movie) => (
-    <MovieCard key={movie.imdbID} movie={movie} />
-  ))}
-</div>
-      </div>
-      <p>Searching for: {searchTerm}</p>
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <p>No movies found</p>
+      )}
     </div>
   );
 }
